4e375fdd50634b9c668e322b5c529ff221695ee5
anggara.setiawan <aanggaadc@gmail.com>
2024-07-19 20:54:38 +0700

    add require types

--- src/app/services/swapi.service.ts
+++ src/app/services/swapi.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { IPeople, IPerson } from '../models';
 
 @Injectable({
   providedIn: 'root',
@@ -10,11 +11,11 @@ export class SwapiService {
 
   constructor(private http: HttpClient) {}
 
-  getPeople(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/people/`);
+  getPeople(): Observable<IPeople> {
+    return this.http.get<IPeople>(`${this.baseUrl}/people/`);
   }
 
-  getPerson(id: number): Observable<any> {
-    return this.http.get(`${this.baseUrl}/people/${id}`);
+  getPerson(id: number): Observable<IPerson> {
+    return this.http.get<IPerson>(`${this.baseUrl}/people/${id}`);
   }
 }
