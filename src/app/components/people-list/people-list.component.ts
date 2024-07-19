913cd29ebf6421aefea94e590992bb5d9d8eb56a
anggara.setiawan <aanggaadc@gmail.com>
2024-07-19 20:54:52 +0700

    integrate all component to api

--- src/app/components/people-list/people-list.component.ts
+++ src/app/components/people-list/people-list.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { SwapiService } from '../../services/swapi.service';
+import { IPerson } from '../../models';
 
 @Component({
   selector: 'app-people-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './people-list.component.html',
-  styleUrl: './people-list.component.scss'
+  styleUrls: ['./people-list.component.scss'],
 })
-export class PeopleListComponent {
+export class PeopleListComponent implements OnInit {
+  people: IPerson[] = [];
+  isLoading = true;
+  isError = false;
 
+  constructor(private swapiService: SwapiService) {}
+
+  ngOnInit(): void {
+    this.fetchPeople();
+  }
+
+  fetchPeople(): void {
+    this.isLoading = true;
+    this.swapiService.getPeople().subscribe({
+      next: (response) => {
+        this.people = response.results;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.log(error);
+        this.isError = true;
+      },
+    });
+  }
 }
--- src/app/components/person-detail/person-detail.component.ts
+++ src/app/components/person-detail/person-detail.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { SwapiService } from '../../services/swapi.service';
+import { IPerson } from '../../models';
 
 @Component({
   selector: 'app-person-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './person-detail.component.html',
-  styleUrl: './person-detail.component.scss'
+  styleUrl: './person-detail.component.scss',
 })
-export class PersonDetailComponent {
+export class PersonDetailComponent implements OnInit {
+  person: IPerson | null = null;
+  isLoading = true;
+  isError = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private swapiService: SwapiService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.fetchPerson(parseInt(id || ''));
+  }
+
+  fetchPerson(id: number): void {
+    this.isLoading = true;
+    this.swapiService.getPerson(id).subscribe({
+      next: (response) => {
+        this.person = response;
+      },
+      error: (error) => {
+        console.log(error);
+        this.isError = error.status;
+        this.isLoading = false;
+      },
+      complete: () => {
+        this.isLoading = false;
+      },
+    });
+  }
 }
