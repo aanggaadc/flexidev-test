dc5c8961c197c9e1b167e282c7e39fbf868d929e
anggara.setiawan <aanggaadc@gmail.com>
2024-07-19 19:35:28 +0700

    add people list and people detail component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PeopleListComponent } from './components/people-list/people-list.component';
+import { PersonDetailComponent } from './components/person-detail/person-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PeopleListComponent },
+  { path: 'person/:id', component: PersonDetailComponent },
+];
